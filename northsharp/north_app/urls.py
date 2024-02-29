2c296c13b58c0281162ec1a46f4b0d77735b8a46
sashag <gogolevsasha031014@mail.ru>
2024-02-29 13:16:54 +0900

    й

--- northsharp/north_app/urls.py
+++ northsharp/north_app/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('', views.index, name='main'),
     path('/about', views.about, name='about'),
     path('/allstudent', views.stud, name='allstud'),
-path('/allempl', views.empl, name='allempl'),
-path('/subj', views.subj, name='subj'),
-path('/grade', views.grade, name='grade')
+    path('/allempl', views.empl, name='allempl'),
+    path('/subj', views.subj, name='subj'),
+    path('/grade', views.grade, name='grade'),
+    path('/form', views.form, name='form')
 ]
