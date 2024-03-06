c2c7f6b181bfcc30b4fee8e7a0332bf0db9f56f0
oVainik <shadowvova2299@gmail.com>
2024-03-06 12:42:36 +0900

    w
    w

--- northsharp/north_app/urls.py
+++ northsharp/north_app/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='main'),
+    path('/about', views.about, name='about'),
+    path('/allstudent', views.stud, name='allstud'),
+    path('/allempl', views.empl, name='allempl'),
+    path('/subj', views.subj, name='subj'),
+    path('/grade', views.grade, name='grade'),
+    path('/form', views.form, name='form')
 ]
