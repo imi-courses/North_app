b295bde149198cd113b70a7daeb6d2555f8cb52d
oVainik <shadowvova2299@gmail.com>
2024-03-06 12:43:20 +0900

    sd
    sd

--- northsharp/north_app/views.py
+++ northsharp/north_app/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
+from .models import Student,Subject,Grade,Table,Employee
 
 # Create your views here.
 def index(request):
     return render(request, "main/index.html")
+
+def about(request):
+    return render(request, "main/about-us.html")
+
+
+def stud(request):
+    Students = Student.objects.all()
+    return render(request, "main/allstudent.html",{'student': Students})
+def empl(request):
+    empl = Employee.objects.all()
+    return render(request, "main/allempl.html",{'empl': empl})
+def subj(request):
+    subj = Subject.objects.all()
+    return render(request, "main/subj.html",{'subj': subj})
+def grade(request):
+    grade = Grade.objects.all()
+    return render(request, "main/grade.html",{'grade': grade})
+
+
+def form(request):
+    if request.user.is_authenticated:
+        context ={
+            'student': Student.objects.all()
+        }
+
+        return render(request, 'main/form.html', context)
+    else:
+        return render(request, 'main/auth.html')
